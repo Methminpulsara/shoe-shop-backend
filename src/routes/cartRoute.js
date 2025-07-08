a5917f54f6e76c015bacf0d3e50d7f504e406ad0
Methminpulsara <methminpulsara10@gmail.com>
2025-07-08 23:20:30 +0530

    update

--- src/routes/cartRoute.js
+++ src/routes/cartRoute.js
@@ -7,6 +7,6 @@ const asyncHandler = require('../utils/asyncHandler');
 // 🔐 Protected Routes (only logged-in users)
 router.post('/', protect, asyncHandler(cartController.addToCart));        
 router.get('/', protect, asyncHandler(cartController.getCart));           
-router.delete('/:id', protect, asyncHandler(cartController.removeItem));    // ❌ Remove specific cart item
+router.delete('/:id', protect, asyncHandler(cartController.removeItem));  
 
 module.exports = router;
